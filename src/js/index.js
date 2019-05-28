75806e77c8fc8c45adb414d1bf7328e92087338f
William L <w-lindvall@outlook.com>
2019-05-28 02:07:25 -0700

    fill select

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,35 @@
-'use strict'
+$(() => {
+  const $sectionSelect = $('#section-select')
+  const sections = [
+    'arts',
+    'automobiles',
+    'books',
+    'business',
+    'fashion',
+    'food',
+    'health',
+    'home',
+    'insider',
+    'magazine',
+    'movies',
+    'national',
+    'nyregion',
+    'obituaries',
+    'opinion',
+    'politics',
+    'realestate',
+    'science',
+    'sports',
+    'sundayreview',
+    'technology',
+    'theater',
+    'tmagazine',
+    'travel',
+    'upshot',
+    'world',
+  ]
 
-console.log('hello')
+  sections.forEach(s =>
+    $sectionSelect.append(`<option value="${s}">${s}</option>`),
+  )
+})
