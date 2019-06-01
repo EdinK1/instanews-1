d531ad2d79de6dcb6855abb18b87ade6948d849d
William L <w-lindvall@outlook.com>
2019-06-01 14:16:20 -0700

    refactor template imports

--- src/js/index.js
+++ src/js/index.js
@@ -50,11 +50,9 @@ $(() => {
   )
 
   const $containerTemplate = document.getElementById('story-grid-template')
-  const containerTemplateContent = document.importNode(
-    $containerTemplate.content,
-    true,
-  )
   // TODO: check for template support
+  const containerTemplateContent = $containerTemplate.content.cloneNode(true)
+
   const $storyList = containerTemplateContent.querySelector('ul')
   $storyList.classList.add('story_list')
   const $storyTemplate = document.getElementById('story-item-template')
@@ -62,10 +60,8 @@ $(() => {
   // lock scroll
   $body.addClass('no-scroll')
 
-  // fetch stories
   $sectionSelect.on('change', ({target}) => {
-    // empty <main>
-    $main.empty()
+    if (!target.value) return
 
     const topStoriesUrl = `https://api.nytimes.com/svc/topstories/v2/${
       target.value
@@ -73,29 +69,22 @@ $(() => {
 
     return $.ajax(topStoriesUrl)
       .done(({results}) => {
-        // iterate over stories
+        $storyList.innerText = ''
+
         results.forEach(({abstract, title, short_url, multimedia}) => {
-          // only render if story has img
           if (!multimedia[0]) return
 
           // import story template
-          const templateContent = document.importNode(
-            $storyTemplate.content,
-            true,
-          )
+          const templateContent = $storyTemplate.content.cloneNode(true)
           const $story = templateContent.querySelector('li')
 
-          // fill w res data
           $story.querySelector('a').setAttribute('href', short_url)
           $story.querySelector('h2').innerText = title
           $story.querySelector('p').innerText = abstract
           $story.style.backgroundImage = `url('${multimedia[0].url}')`
-
-          // append to container
           $storyList.append($story)
         })
 
-        // append to DOM
         $main.append($storyList)
 
         // shrink header
