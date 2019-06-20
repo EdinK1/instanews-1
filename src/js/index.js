e3562fbbc6a03c58868d18fc5c7cf843f8564183
EdinK1 <edinkaymakchi@gmail.com>
2019-06-19 19:27:46 -0700

    fix a comment typo

--- src/js/index.js
+++ src/js/index.js
@@ -1,84 +1,81 @@
-import Loader from './components/Loader'
-import StoryList from './components/StoryList'
-import StoryListItem from './components/StoryListItem'
-import ErrorMessage from './components/ErrorMessage'
-import SectionOption from './components/SectionOption'
+import Loader from "./components/Loader";
+import StoryList from "./components/StoryList";
+import StoryListItem from "./components/StoryListItem";
+import ErrorMessage from "./components/ErrorMessage";
+import SectionOption from "./components/SectionOption";
 
-import SECTIONS from '../constants/sections'
-import NYT_KEY from '../constants/nyt-key'
-import NYT_BASE_URL from '../constants/nyt-base-url'
+import SECTIONS from "../constants/sections";
+import NYT_KEY from "../constants/nyt-key";
+import NYT_BASE_URL from "../constants/nyt-base-url";
 
-import loadImg from './utils/loadImg'
-import setUrlQuery from './utils/setUrlQuery'
-import getUrlQuery from './utils/getUrlQuery'
+import loadImg from "./utils/loadImg";
+import setUrlQuery from "./utils/setUrlQuery";
+import getUrlQuery from "./utils/getUrlQuery";
 
-// polyfill template support
-if (!document.createElement('template').content)
-  import('./polyfills/template')
+// poly fill template support
+if (!document.createElement("template").content) import("./polyfills/template");
 
 // DOM ready
 $(() => {
-  const $body = $('body')
-  const $header = $body.find('.header')
-  const $main = $body.find('main')
-  const $sectionSelect = $('#section-select')
+  const $body = $("body");
+  const $header = $body.find(".header");
+  const $main = $body.find("main");
+  const $sectionSelect = $("#section-select");
 
-  const loader = new Loader()
-  const storyList = new StoryList()
-  const errorMessage = new ErrorMessage()
+  const loader = new Loader();
+  const storyList = new StoryList();
+  const errorMessage = new ErrorMessage();
 
   // fill select options
   SECTIONS.map(val => new SectionOption(val).el).forEach(el =>
-    $sectionSelect.append(el),
-  )
+    $sectionSelect.append(el)
+  );
 
-  $sectionSelect.on('change', ({target}) => {
-    if (!target.value) return
+  $sectionSelect.on("change", ({ target }) => {
+    if (!target.value) return;
 
-    setUrlQuery('section', target.value)
-    $main.append(loader.el)
-    return fetchAndFillStories(target.value)
-  })
+    setUrlQuery("section", target.value);
+    $main.append(loader.el);
+    return fetchAndFillStories(target.value);
+  });
 
   // check for section passed as query string
-  const section = getUrlQuery('section')
+  const section = getUrlQuery("section");
 
   if (section && SECTIONS.includes(section)) {
-    $sectionSelect.val(section)
-    $main.append(loader.el)
-    return fetchAndFillStories(section)
+    $sectionSelect.val(section);
+    $main.append(loader.el);
+    return fetchAndFillStories(section);
   }
 
   function fetchAndFillStories(section) {
-    return $.ajax(
-      `${NYT_BASE_URL}/${section}.json?api-key=${NYT_KEY}`,
-    )
+    return $.ajax(`${NYT_BASE_URL}/${section}.json?api-key=${NYT_KEY}`)
       .always(() => {})
-      .done(({results}) => {
+      .done(({ results }) => {
         const relevantResults = results
           // has image
           .filter(d => d.multimedia[4])
           // limit to 12 stories
-          .slice(0, 12)
+          .slice(0, 12);
 
         const loadingImgs = relevantResults.map(d =>
-          loadImg(d.multimedia[4].url),
-        )
+          loadImg(d.multimedia[4].url)
+        );
         Promise.all(loadingImgs).then(() => {
-          loader.el.remove()
-          $header.addClass('closed')
-          storyList.clear()
+          loader.el.remove();
+          $header.addClass("closed");
+          storyList.clear();
           relevantResults.forEach(d =>
-            storyList.el.appendChild(new StoryListItem(d).el),
-          )
-          $main.append(storyList.el)
-        })
+            storyList.el.appendChild(new StoryListItem(d).el)
+          );
+          $main.append(storyList.el);
+        });
       })
       .fail(() => {
-        loader.el.remove()
-        $header.addClass('closed')
-        storyList.clear()
-        $main.append(errorMessage.el)
-      })
+        loader.el.remove();
+        $header.addClass("closed");
+        storyList.clear();
+        $main.append(errorMessage.el);
+      });
   }
-})
+});
